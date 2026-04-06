ddea1141b90334db58a0b5242e22f44441b2009f
Eduardo <eduman.000@gmail.com>
2026-04-06 15:05:07 -0300

    Fix CartProvider import resolution by using explicit CartContext.jsx path

--- Proyecto/Client/React/client/src/App.jsx
+++ Proyecto/Client/React/client/src/App.jsx
@@ -4,8 +4,9 @@ import Orders from "../Pages/Orders";
 import CategoryPage from "../CategoryPage/CategoryPage";
 import ProtectedRoute from "../ProtectedRoute";
 import Navbar from "../Navbar/Navbar";
-import { CartProvider } from "../Context/CartContext";
+import { CartProvider } from "../Context/CartContext.jsx";
 import AdminPanel from "../Admin/AdminPanel";
+import CheckoutPage from "../Checkout/CheckoutPage";
 
 import { useState, useEffect } from "react";
 import "../Styles/global.css";
@@ -36,6 +37,15 @@ function App() {
             element={
               <ProtectedRoute user={user}>
                 <Orders user={user} />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute user={user}>
+                <CheckoutPage user={user} />
               </ProtectedRoute>
             }
           />
